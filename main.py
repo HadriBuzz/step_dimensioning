dc53398da6a3c91f2a66cc0ed88fe6ce4c109959
HadriBuzz <hadrien.leclerc@mines-ales.org>
2024-03-07 19:12:32 +0100

    commit

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
+st.set_page_config(
+    layout="wide",
+    page_title="Home",
+    page_icon="👋",
+)
+
+st.title("STEP dimensioning")
+
 st.write("Hello world!")
---
+++ pages/1_⚡_energy.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.set_page_config(page_title="Energy wise", page_icon="⚡")
