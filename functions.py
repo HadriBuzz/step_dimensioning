ac684c459007c94ca9687a32625463fde56e48a3
HadriBuzz <hadrien.leclerc@mines-ales.com>
2024-03-11 14:25:38 +0100

    last_friday_commit

--- functions.py
+++ functions.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+
+def sidebar():
+    if "alt_min" not in st.session_state:
+        st.session_state["alt_min"] = 0
+
+    st.session_state["alt_min"] = st.sidebar.number_input('Alt min')
--- ⚙️_Settings.py
+++ ⚙️_Settings.py
@@ -1,15 +1,15 @@
 import streamlit as st
+from functions import sidebar
 
 st.set_page_config(
     layout="wide",
     page_title="Settings",
     page_icon="⚙️",
 )
-
+sidebar()
 st.title("STEP dimensioning")
 
-if "alt_min" not in st.session_state:
-    st.session_state["alt_min"] = 0
+
 st.number_input("Min altitude", value=st.session_state["alt_min"], key="alt_min")
 
 st.write(st.session_state["alt_min"])
