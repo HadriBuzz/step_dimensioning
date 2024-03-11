b1ceed662b36042da32655d44ad70d1bbf51fe25
HadriBuzz <hadrien.leclerc@mines-ales.com>
2024-03-11 15:47:12 +0100

    energy implementation

--- functions.py
+++ functions.py
@@ -2,7 +2,22 @@ import streamlit as st
 
 
 def sidebar():
+
     if "alt_min" not in st.session_state:
         st.session_state["alt_min"] = 0
+    if "alt_max" not in st.session_state:
+        st.session_state["alt_max"] = 0
+    if "fluid_mass" not in st.session_state:
+        st.session_state["fluid_mass"] = 0
 
     st.session_state["alt_min"] = st.sidebar.number_input('Alt min')
+    st.sidebar.write(f"Alt min: {st.session_state['alt_min']} m.")
+
+    st.session_state["alt_max"] = st.sidebar.number_input('Alt max')
+    st.sidebar.write(f"Alt max: {st.session_state['alt_max']} m.")
+
+    st.session_state["fluid_mass"] = st.sidebar.number_input('Fluid mass')
+    st.sidebar.write(f"Mass: {st.session_state['fluid_mass']} kg.")
+
+
+
--- ⚙️_Settings.py
+++ ⚙️_Settings.py
@@ -6,10 +6,17 @@ st.set_page_config(
     page_title="Settings",
     page_icon="⚙️",
 )
+
 sidebar()
+
 st.title("STEP dimensioning")
 
+elevation = st.session_state['alt_max'] - st.session_state['alt_min']
 
-st.number_input("Min altitude", value=st.session_state["alt_min"], key="alt_min")
+st.write(f"Elevation: {elevation} m.")
 
-st.write(st.session_state["alt_min"])
+available_energy = elevation * st.session_state['fluid_mass'] * 9.81
+
+st.write(f"Potential Energy: {available_energy} J.")
+
+st.write(f"Available Energy: {available_energy / 3600} Wh or {available_energy / 3600000} kWh")
