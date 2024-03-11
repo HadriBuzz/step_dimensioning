ac5ece95acbddbc8ee72fa0a76eb2ba28688021c
HadriBuzz <hadrien.leclerc@mines-ales.com>
2024-03-11 15:52:11 +0100

    conversion_rate & time to empy implementation

--- functions.py
+++ functions.py
@@ -9,6 +9,11 @@ def sidebar():
         st.session_state["alt_max"] = 0
     if "fluid_mass" not in st.session_state:
         st.session_state["fluid_mass"] = 0
+    if "conversion_rate" not in st.session_state:
+        st.session_state["conversion_rate"] = 0
+    if "time_to_empty" not in st.session_state:
+        st.session_state["time_to_empty"] = 0
+    
 
     st.session_state["alt_min"] = st.sidebar.number_input('Alt min')
     st.sidebar.write(f"Alt min: {st.session_state['alt_min']} m.")
