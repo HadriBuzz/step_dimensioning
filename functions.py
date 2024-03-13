a0ae20640b16ba03185957254352edeeccd23ce8
HadriBuzz <hadrien.leclerc@mines-ales.com>
2024-03-13 17:53:28 +0100

    conversion & time implementation

--- functions.py
+++ functions.py
@@ -14,13 +14,14 @@ def sidebar():
     if "time_to_empty" not in st.session_state:
         st.session_state["time_to_empty"] = 0
     
-
+    st.sidebar.subheader("Settings")
+    
     st.session_state["alt_min"] = st.sidebar.slider('Alt min (m)', max_value=1000)
    
+    st.session_state["alt_max"] = st.sidebar.slider('Alt max (m)', value =1, max_value=1000)
 
-    st.session_state["alt_max"] = st.sidebar.slider('Alt max (m)', max_value=1000)
+    st.session_state["fluid_mass"] = st.sidebar.slider('Fluid mass (Tons)', min_value=1, max_value=1000000000)
 
-    st.session_state["fluid_mass"] = st.sidebar.number_input('Fluid mass (T)')
+    st.session_state["conversion_rate"] = st.sidebar.slider('Conversion rate (%)', min_value = 50, value=70, max_value=100)
 
-
-
+    st.session_state["time_to_empty"] = st.sidebar.slider('Time to empty energy stock (Hours)', min_value=1, max_value=48)
--- ⚙️_Settings.py
+++ ⚙️_Settings.py
@@ -15,8 +15,10 @@ elevation = st.session_state['alt_max'] - st.session_state['alt_min']
 
 st.write(f"Elevation: {elevation} m.")
 
-available_energy = elevation * st.session_state['fluid_mass'] * 9810
+conv_rate = st.session_state['conversion_rate']
+available_energy = (elevation * st.session_state['fluid_mass'] * 9810)*conv_rate/100
 
-st.write(f"Potential Energy: {available_energy} J.")
+st.write(f"Available/Potential Energy: {available_energy} J or {available_energy / 3600} Wh or {available_energy / 3600000} kWh")
 
-st.write(f"Available Energy: {available_energy / 3600} Wh or {available_energy / 3600000} kWh")
+time_to_empty = st.session_state['time_to_empty']
+st.write(f"Available power: {available_energy / (time_to_empty*3600)} W.")
